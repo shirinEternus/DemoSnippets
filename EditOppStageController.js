f062cd404cc722cc24b52ddba7388329a9a776d4
Shirin <31314803+shirinEternus@users.noreply.github.com>
2019-03-25 15:59:24 +0530

    Add files via upload

---
+++ EditOppStageController.js
@@ -0,0 +1,8 @@
+({
+    saveOpportunity : function(component, event, helper) {
+         helper.saveOpportunity(component, event, helper);
+    },
+    recordUpdated : function(component, event, helper) {
+         helper.recordUpdated(component, event, helper);
+    }
+})
